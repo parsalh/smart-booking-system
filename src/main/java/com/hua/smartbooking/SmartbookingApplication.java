503d0316670725ba7e37f8bd8d0a72eed104391c
Stavroula Parsali <it2022088@hua.gr>
2026-02-18 14:05:55 +0200

    Initial commit

---
+++ src/main/java/com/hua/smartbooking/SmartbookingApplication.java
@@ -0,0 +1,13 @@
+package com.hua.smartbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartbookingApplication.class, args);
+	}
+
+}
